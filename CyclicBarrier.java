9ac5c75d4fac2a37ade737d03d6a04c63ddca29b
fanifish <fanifish@users.noreply.github.com>
2015-02-11 00:45:45 -0600

    Update CyclicBarrier.java
    some trial code

--- CyclicBarrier.java
+++ CyclicBarrier.java
@@ -1,44 +1,86 @@
+package com.utexas.fani;
 
 import java.util.concurrent.Semaphore;
-import java.util.*;
+import java.lang.Object;
+import java.util.Objects;
+import java.util.concurrent.locks.*;
+import java.util.function.*;
 
 public class CyclicBarrier {
-	
+	int numOfThreads;
+	int parties;
+	int arrivalIndex;
+	int x;
+	Semaphore mutex;
+	CountingSemaphore sync;
+
 	public CyclicBarrier(int parties) {
-		// Creates a new CyclicBarrier that will trip when
-		// the given number of parties (threads) are waiting upon it
-		CountingSemaphore semaphore = new CountingSemaphore(parties);
+		numOfThreads = parties;
+		this.parties = parties;
+		mutex = new Semaphore(parties, false); // guarantees first in first out
+		sync = new CountingSemaphore(parties);
 	}
 
+	/**
+	 * waits until all threads have invoked await
+	 * 
+	 * @return
+	 * @throws InterruptedException
+	 */
 	int await() throws InterruptedException {
-		// Waits until all parties have invoked await on this barrier.
-		// If the current thread is not the last to arrive then it is
-		// disabled for thread scheduling purposes and lies dormant until
-		// the last thread arrives.
-		// Returns: the arrival index of the current thread, where index
-		// (parties - 1) indicates the first to arrive and zero indicates
-		// the last to arrive.
-		
-		
-		
-		
+		System.out.println("num of threads calling " + numOfThreads);
+		int arrivalIndex = 0;
+		mutex.acquire();
+		numOfThreads--;
+		arrivalIndex = numOfThreads;
+		if (numOfThreads == 0)
+			sync.V(); // last thread to arrive must notify all
+		mutex.release();
+		while (numOfThreads != 0)
+			sync.P();
+		return arrivalIndex;
 	}
+
+	public static void main(String args[]) throws InterruptedException {
+		CyclicBarrier sync = new CyclicBarrier(5);
+		Thread t[] = new Thread[5];
+		for (int i = 0; i < 5; i += 1) {
+			t[i] = new Thread(new po(i, sync));
+			t[i].start();
+		}
+		
+		System.out.println("Next phase");
+
+		for (int i = 0; i < 1000000000; i += 1) {
+			
+		}
+
+		System.out.println("all released");
+
+	}
+
+}
+
+class po implements Runnable {
+	int x;
+    CyclicBarrier w;
+	po(int n, CyclicBarrier wb) {
+		x = n;
+		w = wb;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("This is thread num " + x);
+		try {
+			System.out.println("The barries release num " + w.await());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	
-	public class CountingSemaphore {
-	    int value;
-	    int origValue;
-	    public CountingSemaphore(int initValue) {
-	        value = initValue;
-	        origValue = initValue;
-	    }
-	    public synchronized void P() throws InterruptedException {
-	        value--;
-	    	while (value != 0) wait();
-	         
-	    }
-	    public synchronized void V() {
-	        value++;
-	        if (value == origValue) notifyAll();
-	    }
-	}
+
 }
